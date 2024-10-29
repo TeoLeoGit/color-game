4297f7537e37b9d2dae8bfcd7a4d7cbfb2ea3ad9
Trương Công Quốc Cường <59004156+TeoLeoGit@users.noreply.github.com>
2024-10-29 23:01:20 +0700

    Add level scripts.

--- Color game/Assets/_Scripts/Datas/AssetData.cs
+++ Color game/Assets/_Scripts/Datas/AssetData.cs
@@ -3,9 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 [CreateAssetMenu(fileName = "AssetData", menuName = "ScriptableObjects/AssetData")]
 public class AssetData : ScriptableObject
 {
---
+++ Color game/Assets/_Scripts/Game/GridAttackPlanner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridAttackPlanner : MonoBehaviour
+{
+    public void StartGridAttack()
+    {
+
+    }
+
+    public void StartPathAttack()
+    {
+
+    }
+
+    public void StartBombAttack()
+    {
+
+    }
+}
